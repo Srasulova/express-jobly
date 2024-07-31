963310dfc3a6e63871889eeff00626ffd64ebf2a
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-07-31 03:04:09 -0400

    add jobs route and integrate them with app

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const { authenticateJWT } = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
 const companiesRoutes = require("./routes/companies");
 const usersRoutes = require("./routes/users");
+const jobsRoutes = require("./routes/jobs"); // Import the jobs routes
 
 const morgan = require("morgan");
 
@@ -24,6 +25,7 @@ app.use(authenticateJWT);
 app.use("/auth", authRoutes);
 app.use("/companies", companiesRoutes);
 app.use("/users", usersRoutes);
+app.use("/jobs", jobsRoutes); // Add the jobs routes
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
